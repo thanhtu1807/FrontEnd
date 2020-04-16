bd6531b1f7157ea5e9635f7b19bd5541c856124a
Thanh Tu <thanhtu180793@gmail.com>
2020-04-16 18:24:33 +0700

    Template engine pug

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.set('views', './views')
+app.set('view engine', 'pug')
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.render('index', {name: 'Tu'}))
+app.get('/users', (req, res) => res.render('users',{
+    users: [
+        {id: 1, name: 'Tu'},
+        {id: 2, name:'Tuan'}
+    ]
+}))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
