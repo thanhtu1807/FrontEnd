48226e2566edd0caaad9fc156dad57e2e81d2d38
Thanh Tu <thanhtu180793@gmail.com>
2020-04-16 21:53:10 +0700

    Post method: create new user func

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
 const port = 3000
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 
 app.set('views', './views')
 app.set('view engine', 'pug')
@@ -11,11 +17,17 @@ users = [
 ]
 // app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/', (req, res) => res.render('index', {name: 'Tu'}))
+
 app.get('/users', (req, res) => res.render('users/index',{users: users,search: ''}))
 app.get('/users/search', (req, res) => {
     const searchWord = req.query.q
     let matchedList = users.filter(user => user.name.toLowerCase().indexOf(searchWord.toLowerCase()) !== -1)
     return res.render('users/index',{users: matchedList, search: searchWord})
+})
+app.get('/users/create', (req, res) => res.render('users/create'))
+app.post('/users/create', (req, res) => {
+    users.push(req.body)
+    res.redirect('/users')
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
