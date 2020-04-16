89508de507e5ce0170b0eb56a23c65a27defc780
Thanh Tu <thanhtu180793@gmail.com>
2020-04-16 21:22:01 +0700

    search function based on query parameter

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -5,13 +5,17 @@ const port = 3000
 app.set('views', './views')
 app.set('view engine', 'pug')
 
+users = [
+    {id: 1, name: 'Tu'},
+    {id: 2, name:'Tuan'},
+]
 // app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/', (req, res) => res.render('index', {name: 'Tu'}))
-app.get('/users', (req, res) => res.render('users',{
-    users: [
-        {id: 1, name: 'Tu'},
-        {id: 2, name:'Tuan'}
-    ]
-}))
+app.get('/users', (req, res) => res.render('users/index',{users: users,search: ''}))
+app.get('/users/search', (req, res) => {
+    const searchWord = req.query.q
+    let matchedList = users.filter(user => user.name.toLowerCase().indexOf(searchWord.toLowerCase()) !== -1)
+    return res.render('users/index',{users: matchedList, search: searchWord})
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
